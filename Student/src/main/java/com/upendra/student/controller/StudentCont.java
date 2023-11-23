931573032400a9de1e8bb9ca8291df3ba8b47385
uxr070301 <uxr070301@darden.com>
2023-11-23 10:16:44 +0530

    tast 2 up

--- Student/src/main/java/com/upendra/student/controller/StudentCont.java
+++ Student/src/main/java/com/upendra/student/controller/StudentCont.java
@@ -1,9 +1,10 @@
 package com.upendra.student.controller;
 
-import java.lang.System.Logger;
-import java.security.PublicKey;
+
 import java.util.List;
-//import com.upendra.student.controller.logger.GlobalLogger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.upendra.student.model.Student;
@@ -21,23 +21,31 @@ import com.upendra.student.service.StudentService;
 @RestController
 @RequestMapping("/student")
 public class StudentCont {
-//	private Logger log = GlobalLogger.getLogger(StudentCont.class);
+
+	Logger logger = LoggerFactory.getLogger(StudentCont.class);
 	@Autowired
 	StudentService studentService;
 	
 	@GetMapping("/getByName/{name}")
 	public List<Student> getBySytudent(@PathVariable("name") String name) {
+		
+		logger.trace("getting student details by name excecuted sucessfully");
+//		 logger.error("An ERROR Message");
 		return studentService.findByName(name);
-//		logger.trace("getting by name");
+		
 	}
 	@GetMapping("/getByAge/{age}")
 	public List<Student> getByAge(@PathVariable("age") int age) {
+		logger.trace("getting students details by age excecuted sucessfully");
+
 		return studentService.findByAge(age);
 		
 	}
 	
 	@GetMapping("/getBySalary/{salary}")
 	public List<Student> getBySalary(@PathVariable("salary") long salary) {
+		logger.trace("getting student detaisl by salary excecuted sucessfully");
+
 		return studentService.findBySalary(salary);
 		
 	}
@@ -45,18 +53,25 @@ public class StudentCont {
 	@PostMapping("/post")
 	public Student post(@RequestBody Student student)
 	{
+		logger.trace("added student details excecuted sucessfully");
+
 		return studentService.post(student);
 	}
 	
 	@PutMapping("/update")
 	public Student updateStudent(@RequestBody Student student)
 	{
+		logger.trace("updated student details excecuted sucessfully");
+
+		
 		return studentService.updateStudent(student);
 		
 	}
 	@DeleteMapping("/delete")
 	public String removeStudent(String name)
 	{
+		logger.trace("deleted student details excecuted sucessfully");
+
 		return studentService.deleteStudent(name);
 		
 	}
