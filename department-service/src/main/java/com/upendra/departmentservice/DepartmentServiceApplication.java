1e4d83b83a354858b63cb5c25bd753c5899a1ec4
uxr070301 <uxr070301@darden.com>
2023-11-26 13:08:48 +0530

    configurations

--- department-service/src/main/java/com/upendra/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/upendra/departmentservice/DepartmentServiceApplication.java
@@ -2,10 +2,10 @@ package com.upendra.departmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DepartmentServiceApplication {
 
 //	@Bean
